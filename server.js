749e810c64c923d0990e9e3e747d092020b76338
Evgenii <xvolkx48@gmail.com>
2018-07-03 13:57:20 +0300

    try global

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 user = require('./user');
 
-var vasya = new user.User("Вася");
-var petya = new user.User("Петя");
+var vasya = new User("Вася");
+var petya = new User("Петя");
 
 vasya.hello(petya);
