b5e077659c3f927d94e7c16d801ce192864ce6bc
Evgenii <xvolkx48@gmail.com>
2018-07-09 15:12:05 +0300

    first echo server

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 var http =require('http');
+var url =require('url');
 
-var server = new http.Server(); //EventEmitter
+var server = new http.Server( function (req, res) {
+    console.log(req.method, req.url);
+
+    var urlParsed=url.parse(req.url, true);
+    console.log(urlParsed);
+
+    if (urlParsed.pathname =='/echo' && urlParsed.query.message){
+        res.end(urlParsed.query.message);
+    }else {
+        res.statusCode = 404;
+        res.end("Page not found");
+    }
+});
 
 server.listen(3000,'127.0.0.1');
-
-var counter = 0;
-
-server.on('request', function (req, res) {
-    res.end("Привет мир!!! " + ++counter);
-});
