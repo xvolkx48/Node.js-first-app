f17afbbbe48e2327abbbbe1e6b3f25340c07c21b
Evgenii <xvolkx48@gmail.com>
2018-07-11 09:22:22 +0300

    new empty project

--- server.js
+++ server.js
@@ -1,9 +1,2 @@
-var http =require('http');
-var debug = require('debug')('server');
-
-var server = http.createServer();
-server.on('request', require('./request'));
-
-server.listen(3000);
-debug("Server is running");
+/*server.js*/
 
