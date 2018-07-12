f1b55b14a3493a6053a326fe23a41239881d1cf4
Evgenii <xvolkx48@gmail.com>
2018-07-12 08:57:28 +0300

    simple chat on Node.js

--- server.js
+++ server.js
@@ -1,2 +1,69 @@
 /*server.js*/
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var chat = require('./chat');
 
+http.createServer(function(req, res) {
+    var urlParsed = url.parse(req.url);
+
+    switch (urlParsed.pathname) {
+        case '/':
+            sendFile("index.html", res);
+            break;
+
+        case '/subscribe':
+            chat.subscribe(req, res);
+            break;
+
+        case '/publish':
+            var body = '';
+
+            req
+                .on('readable', function() {
+                    //body += req.read();
+                    var chunk;
+                    while (null !== (chunk = req.read())) {
+                        body += chunk;
+                    }
+                    if (body.length > 1e4) {
+                        res.statusCode = 413;
+                        res.end("Your message is too big for my little chat");
+                    }
+                })
+                .on('end', function() {
+                    try {
+                        body = JSON.parse(body);
+                    } catch (e) {
+                        res.statusCode = 400;
+                        res.end("Bad Request");
+                        return;
+                    }
+
+                    chat.publish(body.message);
+                    res.end("ok");
+                });
+
+            break;
+
+        default:
+            res.statusCode = 404;
+            res.end("Not found");
+    }
+
+
+}).listen(3000);
+
+
+function sendFile(fileName, res) {
+    var fileStream = fs.createReadStream(fileName);
+    fileStream
+        .on('error', function() {
+            res.statusCode = 500;
+            res.end("Server error");
+        })
+        .pipe(res)
+        .on('close', function() {
+            fileStream.destroy();
+        });
+}
